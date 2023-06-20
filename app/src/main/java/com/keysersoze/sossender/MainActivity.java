32d01cac4149cd0fddfa534bdbdcd147f73e5da1
Kishan Das(kishan.das) <kishan.das@infosys.com>
2023-06-20 21:51:05 +0530

    push successful

--- app/src/main/java/com/keysersoze/sossender/MainActivity.java
+++ app/src/main/java/com/keysersoze/sossender/MainActivity.java
@@ -2,8 +2,9 @@ package com.keysersoze.sossender;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.annotation.SuppressLint;
+import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -17,7 +18,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.Manifest;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String defaultContact;
 
     private static final String TAG = "MainActivity";
+    private static final int PERMISSION_REQUEST_SMS = 123;
+    private static final int PERMISSION_REQUEST_LOCATION = 456;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +49,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         sosButton.setOnClickListener(this);
     }
 
-    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -56,7 +57,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
-
 
     private void sendSOS() {
         String contact = contactEditText.getText().toString();
@@ -78,33 +78,79 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         editor.putString(AppConstants.DEFAULT_CONTACT_KEY, contact);
         editor.apply();
 
+        // Request SMS permission if not granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    PERMISSION_REQUEST_SMS);
+        } else {
+            // Request location permission if not granted
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSION_REQUEST_LOCATION);
+            } else {
+                // Permissions are granted, proceed with sending SOS
+                sendSOSWithLocation(contact);
+            }
+        }
+    }
+
+    private void sendSOSWithLocation(String contact) {
         // Get the user's current location
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "Please grant location permission", Toast.LENGTH_LONG).show();
-            return;
+        if (locationManager != null) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null) {
+                    // Send an SMS with the user's current location to the selected contact
+                    String message = "SOS: My current location is http://maps.google.com/maps?q=" +
+                            location.getLatitude() + "," + location.getLongitude();
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(contact, null, message, null, null);
+
+                    Toast.makeText(this, "SOS message sent to " + contact, Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Unable to get current location", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Toast.makeText(this, "Location permission denied", Toast.LENGTH_LONG).show();
+            }
         }
-
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (location == null) {
-            Toast.makeText(this, "Unable to get current location", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        // Send an SMS with the user's current location to the selected contact
-        String message = "SOS: My current location is http://maps.google.com/maps?q=" +
-                location.getLatitude() + "," + location.getLongitude();
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(contact, null, message, null, null);
-
-        Toast.makeText(this, "SOS message sent to " + contact, Toast.LENGTH_LONG).show();
     }
 
     public static void validatePhoneNumber(String phoneNumber) {
         String regex = "^\\d{10}$";
         if (!phoneNumber.matches(regex)) {
             throw new IllegalArgumentException("Invalid phone number. Please enter a 10-digit number.");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_SMS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // SMS permission granted, proceed with location permission request
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            PERMISSION_REQUEST_LOCATION);
+                } else {
+                    Toast.makeText(this, "SMS permission denied", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case PERMISSION_REQUEST_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Location permission granted, proceed with sending SOS
+                    sendSOSWithLocation(contactEditText.getText().toString());
+                } else {
+                    Toast.makeText(this, "Location permission denied", Toast.LENGTH_LONG).show();
+                }
+                break;
         }
     }
 }
